a3a7874980995323ae8ba63c858b67da042c1b56
Shuṗpo Man <72602326+Shuppin@users.noreply.github.com>
2024-01-05 23:29:42 +0000

    Initial Commit

---
+++ lexer/src/token.rs
@@ -0,0 +1,64 @@
+pub enum TokenKind {
+    // Special
+    Illegal,
+    Eof,
+
+    // Value holders
+    Identifier { name: String },
+    Int(i64),
+    String(String),
+
+    // Arithmetic operators
+    Assign, // =
+    Plus,   // +
+    Minus,  // -
+    Mult,   // *
+    Div,    // /
+    Not,    // !
+    Mod,    // %
+
+    // Comparison operators
+    Less,   // <
+    LessEq, // <=
+    More,   // >
+    MoreEq, // >=
+    Eq,     // ==
+    NotEq,  // !=
+
+    // Delimiters
+    Comma,     // ,
+    Semicolon, // ;
+    Colon,     // :
+
+    // Brackets
+    LParen,   // (
+    RParen,   // )
+    LCurly,   // {
+    RCurly,   // }
+    LBracket, // [
+    RBracket, // ]
+
+    // Keywords
+    Fn,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
+}
+
+impl TokenKind {
+    pub fn lookup_ident(ident: &str) -> TokenKind {
+        match ident {
+            "fn" => TokenKind::Fn,
+            "let" => TokenKind::Let,
+            "if" => TokenKind::If,
+            "else" => TokenKind::Else,
+            "true" => TokenKind::True,
+            "false" => TokenKind::False,
+            "return" => TokenKind::Return,
+            _ => TokenKind::Identifier { name: ident.to_string() },
+        }
+    }
+}

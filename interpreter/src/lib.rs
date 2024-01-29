1ca2ca557147eee55c579b9b1592badfb48d390c
Shuṗpo Man <72602326+Shuppin@users.noreply.github.com>
2024-01-29 17:04:45 +0000

    Add lexer dependency and implement unary expressions

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -1,9 +1,10 @@
 use core::Value;
 
-use ast::expression::Expression;
+use ast::expression::{Expression, Unary};
 use ast::literal::Literal;
 use ast::statement::Statement;
 use ast::Program;
+use lexer::token::TokenKind;
 
 #[cfg(test)]
 mod test;
@@ -26,13 +27,42 @@ fn eval_statement(statement: &Statement) -> Value {
 fn eval_expression(expression: &Expression) -> Value {
     match expression {
         Expression::Literal(literal) => eval_literal(literal),
+        Expression::Unary(unary) => eval_unary_expression(unary),
         _ => todo!(),
+    }
+}
+
+fn eval_unary_expression(unary: &Unary) -> Value {
+    let left = eval_expression(&unary.operand);
+    match unary.operator {
+        TokenKind::Not => eval_not_operation(left),
+        TokenKind::Minus => eval_minus_prefix_operation(left),
+        _ => todo!(),
+    }
+}
+
+fn eval_not_operation(value: Value) -> Value {
+    match value {
+        Value::Integer(n) => Value::Boolean(n == 0),
+        Value::Float(n) => Value::Boolean(n == 0.0),
+        Value::Boolean(b) => Value::Boolean(!b),
+        Value::Null => Value::Boolean(true),
+    }
+}
+
+fn eval_minus_prefix_operation(value: Value) -> Value {
+    match value {
+        Value::Integer(n) => Value::Integer(-n),
+        Value::Float(n) => Value::Float(-n),
+        Value::Boolean(b) => Value::Integer(if b { -1 } else { 0 }),
+        Value::Null => todo!(),
     }
 }
 
 fn eval_literal(literal: &Literal) -> Value {
     match literal {
         Literal::Integer { value, .. } => Value::Integer(*value),
-        _ => todo!()
+        Literal::Boolean { value, .. } => Value::Boolean(*value),
+        _ => todo!(),
     }
 }
--- interpreter/src/test.rs
+++ interpreter/src/test.rs
@@ -19,3 +19,34 @@ fn eval_integer_literals() {
         }
     }
 }
+
+#[test]
+fn eval_boolean_literals() {
+    let tests = [("true", true), ("false", false)];
+
+    for (input, expected_output) in tests {
+        match eval(input) {
+            Value::Boolean(b) => assert_eq!(b, expected_output),
+            _ => panic!("Unexpected value"),
+        }
+    }
+}
+
+#[test]
+fn eval_prefix_operations() {
+    let tests = [
+        ("!true", false),
+        ("!false", false),
+        ("!!true", true),
+        ("!5", false),
+        ("!0", true),
+        ("!!0", false),
+    ];
+
+    for (input, expected_output) in tests {
+        match eval(input) {
+            Value::Boolean(b) => assert_eq!(b, expected_output),
+            _ => panic!("Unexpected value"),
+        }
+    }
+}

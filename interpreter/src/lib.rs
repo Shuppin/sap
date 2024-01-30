6d14405b5aa11228530ae14877d76cffb55b4d62
Shuṗpo Man <72602326+Shuppin@users.noreply.github.com>
2024-01-30 22:50:04 +0000

    Introduce return values and new evaluation outcome type

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -1,35 +1,84 @@
-use core::error::Error;
-use core::Value;
+use core::error::{Error, ErrorKind};
+use core::{err, Value};
+use std::ops::ControlFlow;
 
 use ast::expression::{Binary, Expression, Selection, Unary};
 use ast::literal::Literal;
-use ast::statement::Statement;
+use ast::statement::{Return, Statement};
 use ast::Program;
 use lexer::token::TokenKind;
 
 #[cfg(test)]
 mod test;
 
-pub fn eval(ast: Program) -> Result<Value, Error> {
-    eval_statements(&ast.statements)
+/// Represents a break in traversal of the AST.
+///
+/// The reasons we would want to stop traversal are:
+/// 1. We encountered an error.
+/// 2. The AST contains a return statement, and we need to stop traversal of the current
+///    branch and return the value back to the start of that branch.
+pub enum TraversalBreak {
+    ReturnValue(Value),
+    Error(Error),
 }
 
-fn eval_statements(statements: &Vec<Statement>) -> Result<Value, Error> {
+type EvalOutcome = ControlFlow<TraversalBreak, Value>;
+
+fn map_result_to_eval_outcome(result: Result<Value, Error>) -> EvalOutcome {
+    match result {
+        Ok(value) => ControlFlow::Continue(value),
+        Err(error) => ControlFlow::Break(TraversalBreak::Error(error)),
+    }
+}
+
+pub fn eval(ast: Program) -> Result<Value, Error> {
+    match eval_statements(&ast.statements) {
+        ControlFlow::Continue(value) => Ok(value),
+        ControlFlow::Break(TraversalBreak::ReturnValue(_)) => {
+            err!(ErrorKind::TypeError, "'return' used outside of function")
+        }
+        ControlFlow::Break(TraversalBreak::Error(error)) => Err(error),
+    }
+}
+
+fn eval_statements(statements: &Vec<Statement>) -> EvalOutcome {
     let mut value = Value::Null;
     for statement in statements {
         value = eval_statement(&statement)?;
     }
-    return Ok(value);
+    return ControlFlow::Continue(value);
 }
 
-fn eval_statement(statement: &Statement) -> Result<Value, Error> {
+fn eval_fn_block(statements: &Vec<Statement>) -> EvalOutcome {
+    // A fn block will unwrap return values and just return the inner value
+    match eval_statements(statements) {
+        ControlFlow::Continue(value) => ControlFlow::Continue(value),
+        ControlFlow::Break(TraversalBreak::ReturnValue(value)) => ControlFlow::Continue(value),
+        ControlFlow::Break(TraversalBreak::Error(error)) => {
+            ControlFlow::Break(TraversalBreak::Error(error))
+        }
+    }
+}
+
+fn eval_selection_block(statements: &Vec<Statement>) -> EvalOutcome {
+    // A selection block behaves just the same as a list of statements
+    eval_statements(statements)
+}
+
+fn eval_statement(statement: &Statement) -> EvalOutcome {
     match statement {
         Statement::Expression(expression) => eval_expression(expression),
+        Statement::Return(ret) => eval_return_statement(ret),
         _ => todo!(),
     }
 }
 
-fn eval_expression(expression: &Expression) -> Result<Value, Error> {
+fn eval_return_statement(ret: &Return) -> EvalOutcome {
+    let value = eval_expression(&ret.value)?;
+    return ControlFlow::Break(TraversalBreak::ReturnValue(value));
+}
+
+fn eval_expression(expression: &Expression) -> EvalOutcome {
     match expression {
         Expression::Literal(literal) => eval_literal(literal),
         Expression::Unary(unary) => eval_unary_expression(unary),
@@ -39,35 +88,37 @@ fn eval_expression(expression: &Expression) -> Result<Value, Error> {
     }
 }
 
-fn eval_selection_expression(selection: &Selection) -> Result<Value, Error> {
-    let condition = eval_expression(&selection.condition)?.to_boolean()?;
+fn eval_selection_expression(selection: &Selection) -> EvalOutcome {
+    let result = eval_expression(&selection.condition)?.to_boolean();
+    let condition = map_result_to_eval_outcome(result)?;
     match condition {
         Value::Boolean(b) => {
             if b {
-                eval_statements(&selection.conditional.statements)
+                eval_selection_block(&selection.conditional.statements)
             } else if let Some(else_conditional) = &selection.else_conditional {
-                eval_statements(&else_conditional.statements)
+                eval_selection_block(&else_conditional.statements)
             } else {
-                Ok(Value::Null)
+                ControlFlow::Continue(Value::Null)
             }
         }
         _ => unreachable!(),
     }
 }
 
-fn eval_unary_expression(unary: &Unary) -> Result<Value, Error> {
+fn eval_unary_expression(unary: &Unary) -> EvalOutcome {
     let left = eval_expression(&unary.operand)?;
-    match unary.operator {
+    let result = match unary.operator {
         TokenKind::Not => !left,
         TokenKind::Minus => -left,
         _ => todo!(),
-    }
+    };
+    map_result_to_eval_outcome(result)
 }
 
-fn eval_binary_expression(binary: &Binary) -> Result<Value, Error> {
+fn eval_binary_expression(binary: &Binary) -> EvalOutcome {
     let left = eval_expression(&binary.left)?;
     let right = eval_expression(&binary.right)?;
-    match binary.operator {
+    let result = match binary.operator {
         TokenKind::Plus => left + right,
         TokenKind::Minus => left - right,
         TokenKind::Mult => left * right,
@@ -81,14 +132,16 @@ fn eval_binary_expression(binary: &Binary) -> Result<Value, Error> {
         TokenKind::And => left.and(&right),
         TokenKind::Or => left.or(&right),
         _ => todo!(),
-    }
+    };
+    map_result_to_eval_outcome(result)
 }
 
-fn eval_literal(literal: &Literal) -> Result<Value, Error> {
-    match literal {
+fn eval_literal(literal: &Literal) -> EvalOutcome {
+    let result = match literal {
         Literal::Integer { value, .. } => Ok(Value::Integer(*value)),
         Literal::Float { value, .. } => Ok(Value::Float(*value)),
         Literal::Boolean { value, .. } => Ok(Value::Boolean(*value)),
         _ => todo!(),
-    }
+    };
+    map_result_to_eval_outcome(result)
 }
